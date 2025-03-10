7560c5eb47ad9db19696da840b24ca8cce540d34
Тимур Платонов <gered372@gmail.com>
2025-03-10 10:47:57 +0300

    added second activity, added logging in methods onCreate, onStart etc..

--- app/src/main/java/com/example/platonov/MainActivity.java
+++ app/src/main/java/com/example/platonov/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.platonov;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +22,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Log.i("in method onCreate", "method start");
+        Log.i("in method onCreate", "method end");
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.i("in method onStart", "method start");
+        Log.i("in method onStart", "method end");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.i("in method onResume", "method start");
+        Log.i("in method onResume", "method end");
+    }
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.i("in method onPause", "method start");
+        Log.i("in method onPause", "method end");
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        Log.i("in method onStop", "method start");
+        Log.i("in method onStop", "method end");
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        Log.i("in method onDestroy", "method start");
+        Log.i("in method onDestroy", "method end");
     }
 }
